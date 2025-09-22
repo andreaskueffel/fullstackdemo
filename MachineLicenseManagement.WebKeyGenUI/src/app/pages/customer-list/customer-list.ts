cb28cf1c031e48cc38467d7283f7bac7ac5d0c5d
Andreas Kueffel <andreas.kueffel@praewema.de>
2025-09-22 10:45:44 +0200

    working state of WebUI, first version displaying something from the db :)

--- MachineLicenseManagement.WebKeyGenUI/src/app/pages/customer-list/customer-list.ts
+++ MachineLicenseManagement.WebKeyGenUI/src/app/pages/customer-list/customer-list.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CustomerService } from '../../generated/api-client';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-list',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './customer-list.html',
   styleUrl: './customer-list.scss'
 })
 export class CustomerList {
+  customers: any[] = [];
 
+constructor(private api: CustomerService){
+
+  this.api.customerGetAll().subscribe(data => {
+    console.log("received customers", data);
+    this.customers = data;
+  });
+}
 }
--- MachineLicenseManagement.WebKeyGenUI/src/app/pages/license-list/license-list.ts
+++ MachineLicenseManagement.WebKeyGenUI/src/app/pages/license-list/license-list.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LicenseService } from '../../generated/api-client';
 
 @Component({
   selector: 'app-license-list',
   imports: [
+    CommonModule,
     RouterModule
   ],
   templateUrl: './license-list.html',
   styleUrl: './license-list.scss'
 })
-export class LicenseList {
 
+
+export class LicenseList {
+  licenses: any[] = [];
+  constructor(private api: LicenseService) {
+    api.licenseGetAll().subscribe(data => {
+      console.log("received licenses", data);
+      this.licenses = data;
+
+    });
+
+  }
 }
