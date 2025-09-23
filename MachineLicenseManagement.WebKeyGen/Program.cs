234542abad2a869b8ce9ccdba497feef33524fb2
Andreas Kueffel <andreas.kueffel@praewema.de>
2025-09-23 09:39:19 +0200

    add Authentication and Authorization to WebKeyGen

--- MachineLicenseManagement.WebKeyGen/Program.cs
+++ MachineLicenseManagement.WebKeyGen/Program.cs
@@ -1,11 +1,43 @@
 using MachineLicenseManagement.WebKeyGen;
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton(LiteDbConfig.CreateDatabase());
 builder.Services.AddSingleton<LicenseService>();
 builder.Services.AddControllers();
 builder.Services.AddCors();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        //options.Authority = "http://localhost:8080/realms/edge-realm";
+        options.Authority = "https://test.connect.dvs-technology.com/edge/auth";
+        options.Audience = "edge-frontend-hub";
+        options.RequireHttpsMetadata = true;
+
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            RoleClaimType = "roles"
+
+        };
+        
+        options.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                return System.Threading.Tasks.Task.CompletedTask;
+            },
+            OnTokenValidated = context =>
+            {
+                Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                return System.Threading.Tasks.Task.CompletedTask;
+            }
+        };
+    });
 builder.Services.AddOpenApiDocument(config =>
 {
     config.Title = "LicenseManagement.WebKeyGen API";
@@ -14,6 +46,8 @@ builder.Services.AddOpenApiDocument(config =>
 });
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseOpenApi();
 app.UseSwaggerUi();
 app.MapControllers();
