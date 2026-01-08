92a2cc23c16abb1dfbe6680e770ebb91da47a5a2
Ivanna9 <idzuban5@gmail.com>
2026-01-08 18:21:38 +0100

    Updated routing and configuration

--- MachineLicenseManagement.WebKeyGenUI/src/app/app.routes.ts
+++ MachineLicenseManagement.WebKeyGenUI/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CustomerList } from './pages/customer-list/customer-list';
+import { CustomerForm } from './pages/customer-form/customer-form';
 import { LicenseList } from './pages/license-list/license-list';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'licenses', pathMatch: 'full'},
-    {path: 'licenses', component: LicenseList},
-{path: 'customers', component: CustomerList},
+  { path: '', redirectTo: 'licenses', pathMatch: 'full' },
+  { path: 'licenses', component: LicenseList },
+  { path: 'customers/new', component: CustomerForm },
+  { path: 'customers', component: CustomerList },
 ];
