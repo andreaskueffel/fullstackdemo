09ff36c3f6565e26d56db32ad0289f1e5f44cd43
Ivanna9 <idzuban5@gmail.com>
2026-01-08 18:14:50 +0100

    Added table view with loading indicator to customer list

--- MachineLicenseManagement.WebKeyGenUI/src/app/pages/customer-list/customer-list.ts
+++ MachineLicenseManagement.WebKeyGenUI/src/app/pages/customer-list/customer-list.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
-import { CustomerService } from '../../generated/api-client';
+import { CustomerService } from '../../services/customer.service';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-list',
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   templateUrl: './customer-list.html',
   styleUrl: './customer-list.scss'
 })
 export class CustomerList {
   customers: any[] = [];
+  isLoading = true;
 
-constructor(private api: CustomerService){
+  constructor(private api: CustomerService) {
 
-  this.api.customerGetAll().subscribe(data => {
-    console.log("received customers", data);
-    this.customers = data;
-  });
-}
+    this.api.customerGetAll().subscribe(data => {
+      console.log("received customers", data);
+      this.customers = data;
+      this.isLoading = false;
+    });
+  }
 }
